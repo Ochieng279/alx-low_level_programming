44dc2acac0cf1109e57ab441f5686e913a2480f6
ochieng <linoxotieno001@gmail.com>
2023-03-21 13:36:40 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,32 +16,36 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 
 	}
-
 	else if (n < 0)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
-
 	else if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 
 	}
 	else if (n == 98)
-		printf("%d, ", n);
+		printf("%d", n);
 
 
 
