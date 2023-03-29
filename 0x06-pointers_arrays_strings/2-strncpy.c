bd96e53e38bd5ccea61c7629e3c5971f98b41282
ochieng <linoxotieno001@gmail.com>
2023-03-29 05:40:18 -0700

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_strncpy - function to copy string
+ *@dest: destination string
+ *@src: source string
+ *@n: number of strings
+ *Return: destination string
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+
+}
