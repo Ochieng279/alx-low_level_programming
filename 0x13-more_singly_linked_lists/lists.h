e59bba87f700df4fdd9821804bd46ceb54b226e9
ochieng <linoxotieno001@gmail.com>
2023-05-02 11:20:27 -0700

    2.function that adds a new node at the beginning of a listint_t list.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,6 +16,7 @@ typedef struct listint_s
 } listint_t;
 
 #include <stddef.h>
+#include <stdlib.h>
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
