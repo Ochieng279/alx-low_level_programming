4ecb46bf2e916a3210ec993bb6d231a1034f9358
ochieng <linoxotieno001@gmail.com>
2023-05-02 12:50:13 -0700

    10.function that deletes the node at index index of a listint_t linked list.

--- 0x13-more_singly_linked_lists/10-main.c
+++ 0x13-more_singly_linked_lists/10-main.c
@@ -50,7 +50,7 @@ int main(void)
 	printf("-----------------\n");
 	delete_nodeint_at_index(&head, 0);
 	printf("-----------------\n");
-	elete_nodeint_at_index(&head, 0);
+	delete_nodeint_at_index(&head, 0);
 	printf("-----------------\n");
 	delete_nodeint_at_index(&head, 0);
 	printf("-----------------\n");
